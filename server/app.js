7f9e03905a33efe8d179234416fc4b37ae08c145
Aleksey Bragin <ab@orderbook.net>
2015-07-20 20:13:09 +0300

    Start implementing theming engine (handlebars, compatible with previously imported Ghost's Casper theme).

--- server/app.js
+++ server/app.js
@@ -1,12 +1,28 @@
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var nconf = require('nconf');
+var handlebars = require('handlebars');
 var relativePath = require("./relativepath");
+var themes = require('./themes');
 
 var app = express();
 
-// For development, so it serves build js/css and static files
+// For development: Serve build js/css and static files
 app.use('/assets', express.static(relativePath('../build')));
 app.use('/', express.static(relativePath('../static')));
+
+// Attach Express middleware
+app.use(cookieSession({ keys: ['blah'] }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Theming
+themes.init(handlebars);
+themes.setTheme('casper');
+var appTemplate = handlebars.compile(themes.base);
+console.log(themes.base);
 
 app.get('*', function (req, res, next) {
 	var payload = {
@@ -20,8 +36,17 @@ app.get('*', function (req, res, next) {
 	var title = 'UBlog';
 	var bodyClass = '';
 	var content = 'Loading...';
+	//content = React.renderToString(React.createElement(handler, props));
 
 	var result = '<html></html>';
+
+	var result = appTemplate({
+		content: content,
+		//payload: JSON.stringify(payload),
+		bodyClass: bodyClass,
+		title: title
+	});
+
 
 	res.send(result);
 });
---
+++ server/themes.js
@@ -0,0 +1,103 @@
+var fs = require('fs');
+var path = require('path');
+var relativePath = require('./relativepath');
+
+// Create a wrapper around fs.readFileSync function for easier access
+function read(filename) {
+  return fs.readFileSync(path.join(relativePath('../'), filename), 'utf8');
+}
+
+var themes = {
+
+	helpers: {
+		asset: function (context, options) {
+			console.log('asset helper called!');
+		},
+		author: function (context, options) {
+			console.log('author helper called!');
+		},
+		content: function (context, options) {
+			console.log('content helper called!');
+		},
+		title: function (context, options) {
+			console.log('title helper called!');
+		},
+		date: function (context, options) {
+			console.log('date helper called!');
+		},
+		encode: function (context, options) {
+			console.log('encode helper called!');
+		},
+		excerpt: function (context, options) {
+			console.log('excerpt helper called!');
+		},
+		foreach: function (context, options) {
+			console.log('foreach helper called!');
+		},
+		is: function (context, options) {
+			console.log('is helper called!');
+		},
+		input_password: function (context, options) {
+			console.log('input_password helper called!');
+		},
+		has: function (context, options) {
+			console.log('has helper called!');
+		},
+		navigation: function (context, options) {
+			console.log('navigation helper called!');
+		},
+		page_url: function (context, options) {
+			console.log('page_url helper called!');
+		},
+		pageUrl: function (context, options) {
+			console.log('pageUrl helper called!');
+		},
+		pagination: function (context, options) {
+			console.log('pagination helper called!');
+		},
+		tags: function (context, options) {
+			console.log('tags helper called!');
+		},
+		plural: function (context, options) {
+			console.log('plural helper called!');
+		},
+		url: function (context, options) {
+			console.log('url helper called!');
+		},
+		image: function (context, options) {
+			console.log('image helper called!');
+		}
+	},
+
+	setTheme: function(name) {
+		this.active = name;
+	},
+
+	init: function(hbs) {
+		// Register theme helpers
+		hbs.registerHelper('asset', this.helpers.asset);
+		hbs.registerHelper('author', this.helpers.author);
+		hbs.registerHelper('content', this.helpers.content);
+		hbs.registerHelper('title', this.helpers.title);
+		hbs.registerHelper('date', this.helpers.date);
+		hbs.registerHelper('encode', this.helpers.encode);
+		hbs.registerHelper('excerpt', this.helpers.excerpt);
+		hbs.registerHelper('foreach', this.helpers.foreach);
+		hbs.registerHelper('is', this.helpers.is);
+		hbs.registerHelper('input_password', this.helpers.input_password);
+		hbs.registerHelper('has', this.helpers.has);
+		hbs.registerHelper('navigation', this.helpers.navigation);
+		hbs.registerHelper('page_url', this.helpers.page_url);
+		hbs.registerHelper('pageUrl', this.helpers.pageUrl);
+		hbs.registerHelper('pagination', this.helpers.pagination);
+		hbs.registerHelper('tags', this.helpers.tags);
+		hbs.registerHelper('plural', this.helpers.plural);
+		hbs.registerHelper('url', this.helpers.url);
+		hbs.registerHelper('image', this.helpers.image);
+	},
+
+
+	base: read('themes/' + 'casper' + '/default.hbs') // FIXME: Hardcoded for now
+};
+
+module.exports = themes;
